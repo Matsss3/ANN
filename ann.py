31eaef94885f3cce5e257a35d7a43fa25a555ac4
MatiasCuneo <wyvmqrhjxflcvamrpe@gmail.com>
2023-12-04 13:23:04 -0300

    dataset working

--- ann.py
+++ ann.py
@@ -2,9 +2,18 @@ import numpy as np
 
 np.random.seed(0)
 
-X = [[1, 2, 3, 2.5],
-    [2, 5, -1, 2],
-    [-1.5, 2.7, 3.3, -0.8]]
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -14,11 +23,14 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation = Activation_ReLU()
 
 layer1.forward(X)
-print(layer1.output)
+activation.forward(layer1.output)
 
-layer2.forward(layer1.output)
-print(layer2.output)
+print(layer1.output)
