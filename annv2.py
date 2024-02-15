bf655d794d81fd5ee42e666734467c9681187fc9
MatiasCuneo <wyvmqrhjxflcvamrpe@gmail.com>
2024-02-15 00:06:32 -0300

    dataset and forward

---
+++ annv2.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# np.random.seed(0)
+
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+X, y = spiral_data(100, 3)
+
+# Show spiral data structure
+# plt.scatter(X[:,0], X[:,1], c=y, cmap='brg')
+# plt.show()
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+        
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+        
+class Activation_SoftMax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        
+dl1 = Layer_Dense(2, 3)
+act1 = Activation_ReLU()
+
+dl2 = Layer_Dense(3, 3)
+act2 = Activation_SoftMax()
+
+dl1.forward(X)
+act1.forward(dl1.output)
+
+dl2.forward(act1.output)
+act2.forward(dl2.output)
+
+print(act2.output[:5])
